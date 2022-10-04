bed32b8391e62984ba3db6f64d024a8effd56cc3
Mike Harrison <mharrisonutcoding@gmail.com>
2022-10-03 20:07:20 -0500

    fixed my function for creating the readme file, tried async method didnt work

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,12 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const {writeFile} = require('fs');
-const {generateMarkdown} = require('./utils/generateMarkdown');
+const fs = require('fs').promises;
+const { generateMarkdown } = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = [
+inquirer.prompt([
+
     {
         type: 'input',
         name: 'github',
@@ -50,15 +51,20 @@ const questions = [
     {
         type: 'input',
         name: 'test',
-        message: 'Add specific instructions for testing your project?',
+        message: 'Add specific instructions for testing your project. ',
     },
-];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+])
+    
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
-init();
+// function for creating the readme file
+
+.then((answers) => {
+    const readMeInput = generateMarkdown(answers);
+    fs.writeFile('NewREADME.md', readMeInput, (err) => 
+    err ? console.log(err) : console.log('Your file was created!')
+    );
+});
+
+
